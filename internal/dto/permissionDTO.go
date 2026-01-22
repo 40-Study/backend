357a84914c565a254323d772649581defb241c0e
tvanlee <trongthptdqh@gmail.com>
2026-01-22 16:48:05 +0700

    add: dto permission,role

---
+++ internal/dto/permissionDTO.go
@@ -0,0 +1,28 @@
+package dto
+
+import "github.com/google/uuid"
+
+type UpdatePermissionDTO struct {
+	Description string `json:"description" binding:"required,min=1,max=500"`
+}
+
+type PermissionResponseDTO struct {
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description *string   `json:"description,omitempty"`
+	CreatedAt   string    `json:"created_at"`
+	UpdatedAt   string    `json:"updated_at"`
+}
+
+type PermissionListResponseDTO struct {
+	Permissions []PermissionResponseDTO `json:"permissions"`
+	Total       int64                   `json:"total"`
+	Page        int                     `json:"page"`
+	PageSize    int                     `json:"page_size"`
+}
+
+type PermissionQueryDTO struct {
+	Search   string `query:"search"`
+	Page     int    `query:"page" default:"1"`
+	PageSize int    `query:"page_size" default:"20"`
+}
